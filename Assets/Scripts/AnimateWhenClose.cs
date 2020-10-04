e9d114fc56f8c6519e9e2d48fb8d227ad2b6b74a
limelier <tudor.iacobescu.1@gmail.com>
2020-10-04 13:34:43 +0300

    Add animation based on proximity to camera

--- Assets/Scripts/AnimateWhenClose.cs
+++ Assets/Scripts/AnimateWhenClose.cs
@@ -6,35 +6,35 @@ public class AnimateWhenClose : MonoBehaviour
 {
     private Animation _anim;
     private bool _open = false;
+    private Camera _camera;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _anim = gameObject.GetComponent<Animation>();
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (_anim.isPlaying)
         {
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        var delta = _camera.transform.position - gameObject.transform.position;
+        var distanceMeters = delta.magnitude;
+
+        if (distanceMeters > 0.3 && _open)
         {
-            if (!_open)
-            {
-                Debug.Log("Opening chest...");
-                _anim.Play("open");
-                _open = true;
-            }
-            else
-            {
-                Debug.Log("Closing chest...");
-                _anim.Play("close");
-                _open = false;
-            }
+            _anim.Play("close");
+            _open = false;
+        }
+        else if (distanceMeters < 0.3 && !_open)
+        {
+            _anim.Play("open");
+            _open = true;
         }
     }
 }
